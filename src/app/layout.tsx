44d24f12185ea69554f2a84035197e7a0edebbd4
Syed Ahmad Kumail Zaidi <kumail.zaidi708@gmail.com>
2025-09-19 15:06:10 +0000

    To get your site ready to show ads, copy and paste this code between the

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5530405178327113"
+     crossOrigin="anonymous"></script>
       </head>
       <body className="font-body antialiased h-full">
         <div className="flex flex-col h-full">
