0e6dfe82bcead08bf40f7628893da6809a96899f
Syed Ahmad Kumail Zaidi <kumail.zaidi708@gmail.com>
2025-09-22 16:24:08 +0000

    Below is the Google tag. Add it in the code of every page of this websit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,14 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EK72MNN2R9"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-EK72MNN2R9');
+        `}} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
