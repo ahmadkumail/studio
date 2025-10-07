582d17e60f23c40c80be5e80da843d20dfc0bfe4
ahmadkumail <131776976+ahmadkumail@users.noreply.github.com>
2025-10-08 02:05:35 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,17 +5,24 @@ const nextConfig = {
       {
         source: '/sitemap.xml',
         headers: [
-          { key: 'Content-Type', value: 'application/xml' },
+          {
+            key: 'Content-Type',
+            value: 'application/xml; charset=utf-8',
+          },
         ],
       },
       {
         source: '/robots.txt',
         headers: [
-          { key: 'Content-Type', value: 'text/plain' },
+          {
+            key: 'Content-Type',
+            value: 'text/plain; charset=utf-8',
+          },
         ],
       },
     ];
   },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
